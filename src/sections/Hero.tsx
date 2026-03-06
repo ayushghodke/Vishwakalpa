b248eb322cce63b6aa0d2da9fb768c66909e43fc
ayushghodke <ayushghodke21@gmail.com>
2026-03-06 17:44:57 +0530

    removed scroll-arrow

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -16,9 +16,6 @@ const Hero = () => {
                 </div>
             </div>
 
-            <div className="scroll-indicator">
-                <div className="scroll-arrow"></div>
-            </div>
         </section>
     );
 };
